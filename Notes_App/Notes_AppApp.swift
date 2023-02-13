3f33b67cc182ac2c4be25aa6aebefc78948fbbc1
A-Stroem <asjnyborg@hotmail.com>
2023-02-13 20:39:42 +0100

    Initial Commit

---
+++ Notes_App/Notes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_AppApp.swift
+//  Notes_App
+//
+//  Created by Anders Strøm on 13/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Notes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
