4eb4833108aacbb059b29cae29f2ccd77bff5990
A-Stroem <asjnyborg@hotmail.com>
2023-02-19 14:46:03 +0100

    saves title and text to firebase

--- Notes_App/NoteDetailView.swift
+++ Notes_App/NoteDetailView.swift
@@ -11,17 +11,22 @@ import SwiftUI
 struct NoteDetailView: View {
     @State var note: Note
     var store: NotesStore
+    var fService = FirebaseService()
     
     var body: some View {
         VStack {
             TextField("Title", text: $note.title)
+                .padding(.all)
             TextField("Text", text: $note.text)
+                .padding(.all)
         }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
         .onDisappear {
             for index in 0..<self.store.notes.count {
                 if self.store.notes[index].id == self.note.id {
                     self.store.notes[index] = self.note
                     self.store.saveNotes()
+                    self.fService.addNote(title: self.note.title, text: self.note.text)
                     break
                 }
             }
